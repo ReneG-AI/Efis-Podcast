2758fe17cd8a203e6aaabf4e825d179d7ba05adf
ReneG-AI <sasukekin@hotmail.com>
2025-03-27 20:07:17 +0100

    Configuración mínima para GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/Efis-Podcast',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
